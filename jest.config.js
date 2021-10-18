76649a294741d1b755f32763ea0337e38937f4cb
Jonathan Ortega <jortega0389@gmail.com>
2021-10-18 17:58:24 -0500

    feature(workflows): app jest and coverage

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  projects: [{ preset: 'jest-expo/ios' }, { preset: 'jest-expo/android' }],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.{js,jsx}',
+    '!**/coverage/**',
+    '!**/node_modules/**',
+    '!**/babel.config.js',
+    '!**/jest.setup.js',
+    '!**/jest.config.js',
+  ],
+};
