08bce23f32c43425336c6594cda0d26c2afb448c
Jonathan Ortega <jortega0389@gmail.com>
2021-10-28 20:38:33 -0500

    feat(workflows): adjust gh actions and tests

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,7 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: [
     '**/*.{js,jsx}',
+    '**/*.{ts,tsx}',
     '!**/coverage/**',
     '!**/node_modules/**',
     '!**/babel.config.js',
