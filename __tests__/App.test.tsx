140e0c010484e1a13905d4d6bead57563efea92d
Jonathan Ortega <jortega0389@gmail.com>
2024-12-09 16:58:31 -0500

    chore: adjust lint config

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
-import App from '../App';
+import App from '../app';
 
 describe(`<App>`, () => {
   it(`should render correctly`, () => {
