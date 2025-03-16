cb74b6692cbcc65df694d880bf070b7ac4426790
Jonathan Ortega <jortega0389@gmail.com>
2025-03-15 22:19:43 -0500

    feat: add backgrounds to storybook config

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,12 +1,33 @@
-import type { Preview } from '@storybook/react';
+import type { Preview } from "@storybook/react";
+import { withBackgrounds } from "@storybook/addon-ondevice-backgrounds";
+import { colors } from "../theme/colors";
 
 const preview: Preview = {
+  decorators: [withBackgrounds],
   parameters: {
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/,
       },
+    },
+    backgrounds: {
+      values: [
+        {
+          name: "white",
+          value: "#ffffff",
+        },
+        ...Object.entries(colors)
+          .map(([key, value]) => {
+            return Object.entries(value).map(([shade, color]) => ({
+              name: `${key}-${shade}`,
+              value: color,
+            }));
+          })
+          .flat(),
+      ],
+      default: "white",
+      enabled: true,
     },
   },
 };
