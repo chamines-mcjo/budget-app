dab4677bba9485918a0067ba1c0416d96b9a6d68
Jonathan Ortega <jortega0389@gmail.com>
2025-11-28 21:14:01 -0500

    build: finish storybook 10 upgrade

--- metro.config.js
+++ metro.config.js
@@ -1,14 +1,11 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-// const path = require("path");
 const { getDefaultConfig } = require("expo/metro-config");
 const {
-  withStorybookConfig,
-} = require("@storybook/react-native/metro/withStorybookConfig");
+  withStorybook,
+} = require("@storybook/react-native/metro/withStorybook");
 
-/** @type {import('expo/metro-config').MetroConfig} */
 const defaultConfig = getDefaultConfig(__dirname);
 
-module.exports = withStorybookConfig(defaultConfig, {
+module.exports = withStorybook(defaultConfig, {
   enabled: process.env.STORYBOOK_ENABLED === "true",
-  onDisabledRemoveStorybook: true,
 });
